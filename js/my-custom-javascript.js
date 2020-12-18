da00bacb103f940f7911adb70e26ad716989a0d8
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-12-18 11:22:06 -0600

    Completed Mouse Events Exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,20 +3,22 @@ $(document).ready(function () {
 	alert("The window document is completely ready.");
 });
 
-/** Id Selectors */
-alert($('#page-header').html());
-
-/** Class Selectors */
-$('.codeup').css('border', '1px solid #F00');
-
-/** Element Selectors */
-$('li').css('font-size', '20px');
-alert($('h1').html());
-
-/** Multiple Selectors */
-$('h3').css('background-color', 'yellow')
-$('p').css('background-color', 'yellow')
-$('ul').css('background-color', 'yellow')
-$('h3, p, ul').css('background-color', 'yellow')
+function click(){
+	$(this).css('background-color', 'grey')
+}
+$('#page-header').on('click', click)
 
 
+function dblclick() {
+	$(this).css('font-size', '18px')
+}
+$('p').on('dblclick', dblclick)
+
+
+function hoverOn() {
+	$(this).css('color', '#F00');
+}
+function hoverOff() {
+	$(this).css('color', '#000');
+}
+$('li').hover(hoverOn, hoverOff);
