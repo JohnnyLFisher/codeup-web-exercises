9257778c4ca1f2d92686147854bf4e27d96c5f7f
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-12-24 13:34:12 -0600

    .........

---
+++ js/js-solutions.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/**
+ * Write your solutions here.
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ *
+ * isGreaterThan
+ * isLessThan
+ * isEqualNumber
+ * isEqual
+ * isNot
+ * calcAbs
+ * calcCube
+ * isTypeMatch
+ * findLongestString
+ * isFactor
+ */
+
+// * isGreaterThan
+function isGreaterThan(num1,num2){
+
+}
+
+// * isLessThan
+function isLessThan(num1,num2){
+
+}
+
+// * isEqualNumber
+function isEqualNumber(num1,num2){
+
+}
+
+// * isEqual
+function isEqual(x,y){
+
+}
+
+// * isNot
+function isNot(boolean){
+
+}
+
+// * calcAbs
+function calcAbs(num) {
+
+}
+
+// * calcCube
+function calcCube (num){
+
+}
+
+// * isTypeMatch
+function isTypeMatch(type1,type2){
+
+}
+
+// * findLongestString
+function findLongestString (string1,string2) {
+
+}
+
+// * isFactor
+function isFactor(num1,num2){
+
+}
