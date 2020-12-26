82bfccbf1c0ddb53f0be877824c7f329e232baa2
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-12-25 23:51:51 -0600

    qwerty

--- js/js-solutions.js
+++ js/js-solutions.js
@@ -21,50 +21,71 @@
 
 // * isGreaterThan
 function isGreaterThan(num1,num2){
-
+	return parseFloat(num1) > parseFloat(num2)
 }
 
 // * isLessThan
 function isLessThan(num1,num2){
-
+	return parseFloat(num1) < parseFloat(num2)
 }
 
 // * isEqualNumber
 function isEqualNumber(num1,num2){
-
+	return parseFloat(num1) === parseFloat(num2)
 }
 
 // * isEqual
 function isEqual(x,y){
-
+	return x == y
 }
 
 // * isNot
 function isNot(boolean){
-
+	if (boolean == null){
+		return false
+	} else {
+		return !(boolean)
+	}
 }
 
 // * calcAbs
 function calcAbs(num) {
-
+	if (typeof (num) !== "number") {
+		return 0
+	} else {
+		return Math.abs(num)
+	}
 }
 
 // * calcCube
 function calcCube (num){
-
+	if (typeof (num) !== "number") {
+		return 0
+	} else {
+		return Math.pow(num,3)
+	}
 }
 
 // * isTypeMatch
 function isTypeMatch(type1,type2){
-
+	return typeof (type1) === typeof (type2)
 }
 
 // * findLongestString
 function findLongestString (string1,string2) {
-
+	if (typeof string1 !== "string" && typeof string2 !== "string"){
+		return ""
+	} else if (typeof string1 === "string" && typeof string2 !== "string"){
+		return string1
+	} else if (typeof string1 !== "string" && typeof string2 === "string"){
+		return string2
+	} else if (string1.length > string2.length) {
+		return string1
+	} else
+		return string2
 }
 
 // * isFactor
 function isFactor(num1,num2){
-
+	return ((parseFloat(num2) % parseFloat(num1)) === 0)
 }
