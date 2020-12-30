7ee12817ca7b4ddea9e3e51e0740739d08e9887a
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-12-29 21:13:46 -0600

    WASD

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@
 console.log("Hello World!");
 
 alert("You just won a new iPad!")
-alert("This TOTALLY isn't a scam")
+alert("This TOTALLY isn't a scam...")
+prompt("Just fill out your address and we'll send it right to your door!")
