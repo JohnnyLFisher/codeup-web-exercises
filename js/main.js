43cd18c9bbc9c38285886319b62e4421548870d8
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-03-06 21:53:19 -0600

    personal nerd stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
-"use strict";
+$(document).ready(function() {
+	let base = 250;
+	let previous = [];
 
-console.log("Hello World!");
+	let ascended = true;
+	let info;
+	if (ascended === true) {
+		info = 0
+	} else {
+		info = 1
+	}
 
-alert("You just won a new iPad!")
-alert("This TOTALLY isn't a scam...")
-prompt("Just fill out your address and we'll send it right to your door!")
+	let fOA = ["The Fruit of Ascension", "The ????? of ?????????"]
+	let prime = ["Primordial", "Unknown"]
+	let rankUp = ["Past the Mortal Boundary, strength is measured very differently. Rather than leveling up through the gaining of experience, Power is cultivated through the active absorption of higher energies. There are 10 Immortal Ranks separated by 9 half ranks.", "Further information is restricted."]
+
+	for (let i = 1; i <= 56; i++) {
+		if( i === 1){
+			let exp = base;
+			previous.push(exp);
+
+			$("div").append(`<br>To go from level ${i - 1} to level ${i} requires ${commas(exp)} exp.`);
+
+		} else  if (i <= 55){
+			let exp = previous.reduce((a, b) => a + b, 0) + (base * i);
+			previous.push(exp);
+
+			$("div").append(`<br>To go from level ${i - 1} to level ${i} requires ${commas(exp)} exp.`);
+
+		} else {
+
+			$("div").append(`<br><br>Level 55 is the Mortal Boundary. Those who have crossed the Mortal Boundary cannot be slain by those who have not.`);
+			$("div").append(`<br>To pass the Mortal Boundary requires "<em>${fOA[info]}</em>" - Item class: ${prime[info]}.`);
+			$("div").append(`<br><br>${rankUp[info]}`);
+
+		}
+	}
+
+	function commas(x) {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+
+});
